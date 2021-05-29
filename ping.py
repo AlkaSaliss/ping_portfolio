0feeaa6941f5d70c5f7c680e828fce2a3679d1d6
Mahamadou Salissou Aboubacar Alka <alkasalissou@gmail.com>
2021-05-29 03:28:48 +0200

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,3 +1,6 @@
 import urllib.request
-tmp = urllib.request.urlopen("https://alkai.netlify.app/", timeout=60)
+tmp = urllib.request.urlopen("https://alkai.herokuapp.com", timeout=60)
+tmp = urllib.request.urlopen("https://alkai.netlify.app", timeout=60)
+
+
 print(tmp.readlines())
